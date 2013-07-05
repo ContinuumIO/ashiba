325f25ed6d6d9bcf8b776c3d073f25a90fe64a5c
Clayton Davis <claydavi@indiana.edu>
2013-07-05 02:15:22 -0500

    Add support for more input types

--- ashiba_share/compiled_project_files/static/ashiba.js
+++ ashiba_share/compiled_project_files/static/ashiba.js
@@ -1,12 +1,14 @@
 //Should probably namespace this
 var ashiba = {
   'getDom' : function(){
+    var dom = {}; 
     var inputs = $('input');
-    var dom = {};
     for(i=0;i<inputs.length;i++){
       if(!!inputs[i].id){
         var element = inputs[i];
-        if(element.type === "text"){
+        if(element.type === "text" ||
+           element.type === "range"||
+           element.type === "date"){
           dom[element.id] = {};
           dom[element.id]['value'] = element.value;
         } else if(element.type === "checkbox"){
@@ -15,13 +17,22 @@ var ashiba = {
         }
       }
     }
+    var selects = $('select');
+    for(i=0;i<selects.length;i++){
+      if(!!selects[i].id){
+        var element = selects[i];
+        dom[element.id] = {};
+        dom[element.id]['value'] = element.value;
+      }
+    }
+
     return JSON.stringify(dom);
   },
 
   'setDom' : function (jsonResponse){
     var domObj = jsonResponse["dom_changes"];
     Object.getOwnPropertyNames(domObj).forEach(function(element_name){
-      var element = $("#"+element_name)[0];
+      var element = document.getElementById(element_name);
       Object.getOwnPropertyNames(domObj[element_name]).forEach(function(property){
         console.log("Element name: " + element_name + '\n'
                   + "    Property: " + property + '\n'
@@ -51,3 +62,14 @@ var ashiba = {
     };
   }
 }
+
+$(document).ready(function(){
+  // Nice-ify sliders
+  $('input[type="range"]').after(function(){
+    return ('&nbsp;<output for="' + this.id +'">' +
+                    this.value +'</output>');
+  }).on('change', function(){
+    $('output[for="'+ this.id +'"]').val(this.value);
+  });
+
+});
