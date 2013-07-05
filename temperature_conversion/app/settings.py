4dd89443a8a7346bb43e9c06907324df7640348e
Clayton Davis <claydavi@indiana.edu>
2013-07-05 02:15:48 -0500

    Delete compiled app

--- temperature_conversion/app/settings.py
+++
@@ -1,4 +0,0 @@
-APP_NAME = "Temperature Converter"
-APP_ICON = "static/img/icon128.png"
-def test__function():
-    return True
--- temperature_conversion/app/static/ashiba_compiled.js
+++
@@ -1,7 +0,0 @@
-/* Compiled with Ashiba v0.0 */
-
-$(window).load(function(){
-  $("#btn_convert").on("click",
-    ashiba.eventHandlerFactory("btn_convert", "click")
-  );
-});
