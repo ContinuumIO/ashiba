cde0578cdf603aaddecbe92f544f68346ddc0af5
Clayton Davis <claydavi@indiana.edu>
2013-07-30 18:14:25 -0500

    Renamed to handlers.py

--- ashiba/share/new_project_files/events.py
+++
@@ -1,7 +0,0 @@
-## This file is where you write your event handlers in Python.
-## Define functions named like element_id__event(dom) e.g.
-#   btn_mybutton__click(dom):
-#       dom['txt_textbox']['value'] = "Button clicked."
-#   return dom
-#
-## You can delete this comment once you've read it.
