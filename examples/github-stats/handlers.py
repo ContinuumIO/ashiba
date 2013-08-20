a3c8a01800683c3af8838c17a7861064e71fd140
Ghostface-jr <xacadena@gmail.com>
2013-08-20 11:09:36 -0500

    fixed example code

--- examples/github-stats/handlers.py
+++ examples/github-stats/handlers.py
@@ -1,26 +1,15 @@
 import ashiba   
 from ashiba.plot import plt
 
-from github import daily_commit_timeline, weekly_commit_timeline
+from github import daily_commit_timeline, weekly_commit_timeline, additions_deletions_per_week
 
 def mybutton__click(dom):
 
     user = dom['usersearch']['value']
     repo = dom['reposearch']['value']
 
-<<<<<<< HEAD
     ts = weekly_commit_timeline(user, repo)
     ts2 = daily_commit_timeline(user, repo)
-=======
-    dom['my_tabs'].add_tab(user, repo)
-
-    #ts = weekly_commit_timeline(user, repo)
-    #ts2 = daily_commit_timeline(user, repo)
-    import pandas as pd
-    import numpy as np
-    s1 = pd.Series(np.arange(1,5)) 
->>>>>>> a78e5c5ab979d2dd2f5c40c4d86eb5efa0f93cec
-    
 
     ts.plot()
     plt.title("Weekly commits for %s".format('Github') %repo)
